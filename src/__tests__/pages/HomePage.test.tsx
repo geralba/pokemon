4281c95f314562c933de51cbd485b5dc52298827
geralba <geralba.kalbaj1@gmail.com>
2025-01-11 14:03:02 +0100

    chore: initial commit

--- src/__tests__/pages/HomePage.test.tsx
+++ src/__tests__/pages/HomePage.test.tsx
@@ -1,15 +1,11 @@
-// !STARTERCONF You should delete this page
-
 import { render, screen } from '@testing-library/react';
 
 import HomePage from '@/app/page';
 
-describe('Homepage', () => {
-  it('renders the Components', () => {
+describe('HomePage Component', () => {
+  it('should render the heading', () => {
     render(<HomePage />);
-
-    const heading = screen.getByText(/A starter for Next.js/i);
-
+    const heading = screen.getByText(/This is HomePage/i);
     expect(heading).toBeInTheDocument();
   });
 });
